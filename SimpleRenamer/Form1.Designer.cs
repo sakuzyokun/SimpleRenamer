ce9d86da164b2f7a01a3637263aea9e30d833fe0
sakuzyokun <sakuzyokun.sakuzyo0606@gmail.com>
2025-08-22 15:18:54 +0900

    Add files via upload

---
+++ SimpleRenamer/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SimpleRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAddFiles = new System.Windows.Forms.Button();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnRemoveSelected = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.listBoxPreview = new System.Windows.Forms.ListBox();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            this.numericStart = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddFiles
+            // 
+            this.btnAddFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnAddFiles.Name = "btnAddFiles";
+            this.btnAddFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFiles.TabIndex = 0;
+            this.btnAddFiles.Text = "ファイル追加";
+            this.btnAddFiles.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.Location = new System.Drawing.Point(93, 12);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFolder.TabIndex = 1;
+            this.btnAddFolder.Text = "フォルダ追加";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(174, 12);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 2;
+            this.btnPreview.Text = "プレビュー";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(255, 12);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "一括リネーム";
+            this.btnRename.UseVisualStyleBackColor = true;
+            // 
+            // btnRemoveSelected
+            // 
+            this.btnRemoveSelected.Location = new System.Drawing.Point(336, 12);
+            this.btnRemoveSelected.Name = "btnRemoveSelected";
+            this.btnRemoveSelected.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveSelected.TabIndex = 4;
+            this.btnRemoveSelected.Text = "選択削除";
+            this.toolTip1.SetToolTip(this.btnRemoveSelected, "選択した項目のみ削除します");
+            this.btnRemoveSelected.UseVisualStyleBackColor = true;
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(417, 12);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
+            this.btnClearAll.TabIndex = 5;
+            this.btnClearAll.Text = "全削除";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.HorizontalScrollbar = true;
+            this.listBoxFiles.ItemHeight = 12;
+            this.listBoxFiles.Location = new System.Drawing.Point(12, 66);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.ScrollAlwaysVisible = true;
+            this.listBoxFiles.Size = new System.Drawing.Size(237, 184);
+            this.listBoxFiles.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.listBoxFiles, "変更する前のファイル名");
+            // 
+            // listBoxPreview
+            // 
+            this.listBoxPreview.FormattingEnabled = true;
+            this.listBoxPreview.HorizontalScrollbar = true;
+            this.listBoxPreview.ItemHeight = 12;
+            this.listBoxPreview.Location = new System.Drawing.Point(255, 66);
+            this.listBoxPreview.Name = "listBoxPreview";
+            this.listBoxPreview.ScrollAlwaysVisible = true;
+            this.listBoxPreview.Size = new System.Drawing.Size(237, 184);
+            this.listBoxPreview.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.listBoxPreview, "変更後のファイル名(プレビュー)");
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(12, 41);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(156, 19);
+            this.txtPrefix.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.txtPrefix, "数字の前に入れる文字");
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Location = new System.Drawing.Point(336, 42);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(156, 19);
+            this.txtSuffix.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.txtSuffix, "数字の後に入れる文字");
+            // 
+            // numericStart
+            // 
+            this.numericStart.Location = new System.Drawing.Point(174, 42);
+            this.numericStart.Name = "numericStart";
+            this.numericStart.Size = new System.Drawing.Size(156, 19);
+            this.numericStart.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.numericStart, "連番数字\r\nここに数字を入力すると入力した数値から始まります");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 262);
+            this.Controls.Add(this.numericStart);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.listBoxPreview);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnRemoveSelected);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnAddFolder);
+            this.Controls.Add(this.btnAddFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SimpleRenamer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericStart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddFiles;
+        private System.Windows.Forms.Button btnAddFolder;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnRemoveSelected;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.ListBox listBoxPreview;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.NumericUpDown numericStart;
+    }
+}
+
---
+++ SimpleRenamer/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SimpleRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // イベント登録
+            btnAddFiles.Click += BtnAddFiles_Click;
+            btnAddFolder.Click += BtnAddFolder_Click;
+            btnPreview.Click += BtnPreview_Click;
+            btnRename.Click += BtnRename_Click;
+            btnRemoveSelected.Click += BtnRemoveSelected_Click;
+            btnClearAll.Click += BtnClearAll_Click;
+        }
+
+        private void BtnAddFiles_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var file in ofd.FileNames)
+                        listBoxFiles.Items.Add(file);
+                }
+            }
+        }
+
+        private void BtnAddFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var file in Directory.GetFiles(fbd.SelectedPath))
+                        listBoxFiles.Items.Add(file);
+                }
+            }
+        }
+
+        private void BtnPreview_Click(object sender, EventArgs e)
+        {
+            listBoxPreview.Items.Clear();
+            int startNum = (int)numericStart.Value;
+
+            for (int i = 0; i < listBoxFiles.Items.Count; i++)
+            {
+                string oldPath = listBoxFiles.Items[i].ToString();
+                string dir = Path.GetDirectoryName(oldPath);
+                string ext = Path.GetExtension(oldPath);
+                string newName = $"{txtPrefix.Text}{startNum + i}{txtSuffix.Text}{ext}";
+                listBoxPreview.Items.Add(Path.Combine(dir, newName));
+            }
+        }
+
+        private void BtnRename_Click(object sender, EventArgs e)
+        {
+            int startNum = (int)numericStart.Value;
+
+            for (int i = 0; i < listBoxFiles.Items.Count; i++)
+            {
+                string oldPath = listBoxFiles.Items[i].ToString();
+                string dir = Path.GetDirectoryName(oldPath);
+                string ext = Path.GetExtension(oldPath);
+                string newPath = Path.Combine(dir, $"{txtPrefix.Text}{startNum + i}{txtSuffix.Text}{ext}");
+
+                try
+                {
+                    File.Move(oldPath, newPath);
+                    listBoxFiles.Items[i] = newPath; // リスト更新
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"リネーム失敗: {oldPath}\n{ex.Message}");
+                }
+            }
+
+            MessageBox.Show("リネーム完了！");
+            BtnPreview_Click(null, null); // プレビュー更新
+        }
+
+        private void BtnRemoveSelected_Click(object sender, EventArgs e)
+        {
+            while (listBoxFiles.SelectedItems.Count > 0)
+                listBoxFiles.Items.Remove(listBoxFiles.SelectedItems[0]);
+        }
+
+        private void BtnClearAll_Click(object sender, EventArgs e)
+        {
+            listBoxFiles.Items.Clear();
+            listBoxPreview.Items.Clear();
+        }
+
+        /*[STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }*/
+    }
+}
